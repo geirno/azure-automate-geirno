b4be6e392efbbd71bdf744b9ff2ff12303b35f16
geirno <93124565+geirno@users.noreply.github.com>
2021-11-20 18:15:43 +0100

    oppgave 3 endret

--- src/Oppgave_3.ps1
+++ src/Oppgave_3.ps1
@@ -4,6 +4,6 @@ $cards = $response.Content | ConvertFrom-Json
 
 $kortstokk = @()
 ForEach ($card in $cards) {
-    $kortstokk += $kortstokk + ($card.suit[0] + $card.value)
+    $kortstokk += ($card.suit[0] + $card.value)
 }
 Write-Host "Kortstokk : $Kortstokk"
